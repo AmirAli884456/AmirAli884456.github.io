91e1bf49162ccf53519754f422c966b13ed33025
AmirAli884456 <139352780+AmirAli884456@users.noreply.github.com>
2023-07-17 15:54:17 +0330

    Update script.js

--- 05-1/script.js
+++ 05-1/script.js
@@ -1,4 +1,6 @@
-let n1=document.getElementById('no1'), n2=document.getElementById('no2'),n3=document.getElementById('res'), add=documnt.getElementById('add');
+let n1=document.getElementById('no1'),
+let n2=document.getElementById('no2')
+    n3=document.getElementById('res'), add=documnt.getElementById('add');
 add.onclick=function(){e
     n3.value = n2.value +  n1.value
 }
